a0215dbb2181b52d5668970b68785f25374c5ed1
trdmonkey <trdmonkey218@gmail.com>
2024-03-04 21:01:12 -0500

    subir proyecto a git

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function() {
+
+    // canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class inputHandler {
+
+    }
+    // projectile, particle, player
+    class projectile {
+
+    }
+    class particle {
+
+    }
+    class player {
+        
+    }
+
+});
